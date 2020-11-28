61f818509cda46446d71f1df3ef6ed43d9e8567f
cflin <cflin@mail.cjcu.edu.tw>
2020-11-28 13:52:44 +0800

    旋轉

--- test10.py
+++ test10.py
@@ -5,8 +5,12 @@ rows, cols=img.shape[:2]
 # 下儑的 None 本应䖔是䥂出图像的尺寸虽但是因为后䥨我们䕭置了缩放因子
 # 放大
 #res=cv2.resize(img,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
+#dst=cv2.resize(img,None,fx=1/2,fy=1/2,interpolation=cv2.INTER_CUBIC)
 # 平移
-M = np.float32([[1,0,100],[0,1,50]])
+#M = np.float32([[1,0,100],[0,1,50]])
+#dst = cv2.warpAffine(img,M,(cols,rows))
+#旋轉
+M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
 dst = cv2.warpAffine(img,M,(cols,rows))
 
 while(1):
