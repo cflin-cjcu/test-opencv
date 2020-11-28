a6e176f38471891bcbdf2dfaee9c604fe6365953
cflin <cflin@mail.cjcu.edu.tw>
2020-11-28 13:37:30 +0800

    平移

---
+++ test10.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+img=cv2.imread('test1.jpg')
+rows, cols=img.shape[:2]
+# 下儑的 None 本应䖔是䥂出图像的尺寸虽但是因为后䥨我们䕭置了缩放因子
+# 放大
+#res=cv2.resize(img,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
+# 平移
+M = np.float32([[1,0,100],[0,1,50]])
+dst = cv2.warpAffine(img,M,(cols,rows))
+
+while(1):
+    cv2.imshow('res',dst)
+    cv2.imshow('img',img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
--- test9.py
+++ test9.py
@@ -3,13 +3,15 @@ import numpy as np
 img=cv2.imread('test1.jpg')
 # 下儑的 None 本应䖔是䥂出图像的尺寸虽但是因为后䥨我们䕭置了缩放因子
 # 因此䦈䭻为 None
-res=cv2.resize(img,None,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
+res=cv2.resize(img,fx=2,fy=2,interpolation=cv2.INTER_CUBIC)
 #OR
 # 䦈䭻呢虽我们直接䕭置䥂出图像的尺寸虽所以不用䕭置缩放因子
 height,width=img.shape[:2]
+print(img.shape)
 res=cv2.resize(img,(2*width,2*height),interpolation=cv2.INTER_CUBIC)
 while(1):
     cv2.imshow('res',res)
+    cv2.imwrite('test3.jpg',res)
     cv2.imshow('img',img)
     if cv2.waitKey(1) & 0xFF == 27:
         break
